5b247dfb0c70ea2eae1996bcbee1c264f3c5abd7
andretabourian@hotmail.com <andretabourian@hotmail.com>
2022-04-29 17:04:17 +0200

    Added a device method to calculate subsets sequentially.

--- src/combination.cu
+++ src/combination.cu
@@ -57,6 +57,60 @@ __device__ uint64_t ith_combination(uint64_t n, uint64_t r, uint64_t i) {
 
 }
 
+/**
+ * O(n^2 + n) - ~ 64*64 + 64
+ */
+__device__ uint64_t ith_subset(uint64_t mask, uint64_t i) {
+
+    uint64_t lim = 0;
+
+    for(uint32_t comb=1; comb < __popcll(mask); comb++) {
+
+        uint32_t ncr = nCr(__popcll(mask), comb);
+
+        if(i < lim + ncr) {
+
+            uint64_t submask = 0;
+            uint64_t subcomb = ith_combination(__popcll(mask), comb, i - lim);
+
+            while(subcomb != 0) {
+
+                uint32_t pos = __ffsll(subcomb);
+
+                subcomb = subcomb >> pos;
+
+                if((subcomb & 1) == 1) {
+                    
+                    uint32_t filter = 1llu;
+                    uint64_t tmp = mask;
+
+                    for(uint32_t nshifts = 0; nshifts < pos + 1; nshifts++) {
+                        
+                        filter = filter << __ffsll(mask);
+                        tmp = tmp >> (__ffsll(tmp) + 1) << (__ffsll(tmp) + 1);
+
+                    }
+
+                    submask = submask | filter;
+
+                }
+
+                subcomb = (subcomb >> 1) << (pos + 1);
+            
+            }
+
+            return submask;
+
+        }
+
+        lim = lim + ncr;
+
+    }
+
+	return 0;
+
+}
+
 __device__ __host__ void print_mask(uint64_t mask, uint32_t size) {
 
     uint64_t bit = 1ll << (size - 1);
