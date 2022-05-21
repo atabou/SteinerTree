31a49d2c29620137a3c9fe87d94a20c40fbfcd99
atabou <andretabourian@hotmail.com>
2022-05-21 14:25:15 +0300

    Updated table printing format.

--- src/table.c
+++ src/table.c
@@ -21,32 +21,32 @@ table_t* make_table(int32_t n, int32_t m) {
 
 void print_table(table_t* table) {
     
-    printf("\n\033[0;32m   |");
+    printf("\n\033[0;32m    |");
 
     for(int32_t i=0; i<table->m; i++) {
-        printf("%4d|", i);
+        printf("%3d|", i);
     }
 
     printf("\n");
     for(int32_t i=0; i<table->m; i++) {
-        printf("+----");
+        printf("+---");
     }
-    printf("+----+\033[0m\n");
+    printf("+---+\033[0m\n");
 
     for(int32_t i=0; i<table->n; i++) {
-        printf("\033[0;32m %4d|\033[0m", i);
+        printf("\033[0;32m %3d|\033[0m", i);
         for(int32_t j=0; j<table->m; j++) {
 
             if(table->vals[i * table->m + j] == FLT_MAX) {
-                printf("\033[0;31m%4d\033[0m|", -1);
+                printf("\033[0;31m%3d\033[0m|", -1);
             } else {
-                printf("%4f|", table->vals[i * table->m + j]);
+                printf("%.1f|", table->vals[i * table->m + j]);
             }
             
         }
-        printf("\n\033[0;32m+----+\033[0m");
+        printf("\n\033[0;32m+---+\033[0m");
         for(int32_t j=0; j<table->m; j++) {
-            printf("----+");
+            printf("---+");
         }
         printf("\n");
     }
