b1c13454ddfb56c43610010048b5e78b269e6abe
atabou <andretabourian@hotmail.com>
2022-06-02 01:15:27 +0300

    Modified steiner kernel to a different implementation.

--- src/steiner.gpu.cu
+++ src/steiner.gpu.cu
@@ -13,6 +13,8 @@
 #define BLOCK_1D_SIZE 1024
 #define MAX_BLOCKS 65536
 
+#define BLOCK_SIZE 1024
+
 
 __global__ void dw_fill_base_cases(cudatable::table_t* costs, cudagraph::graph_t* g, cudatable::table_t* distances, cudaquery::query_t* terminals) {
 
@@ -21,14 +23,14 @@ __global__ void dw_fill_base_cases(cudatable::table_t* costs, cudagraph::graph_t
         + blockIdx.x * blockDim.x  // Number of threads inside the 1D part of the grid coming before the thread in question.
         + threadIdx.x; // The position of the thread in the block
 
-    if(thread_id < terminals->size * costs->n) {
+    if(thread_id < terminals->size * costs->m) {
+        
+        int32_t v = thread_id % costs->m;
+        uint64_t mask = 1llu << (thread_id / costs->m);
 
-        int32_t v = thread_id / terminals->size;
-        uint64_t mask = 1llu << (thread_id % terminals->size);
-        
         int32_t u = terminals->vals[terminals->size - __ffsll(mask)];
-        
-        costs->vals[v * costs->m + (mask - 1)] = distances->vals[v * distances->m + u];
+
+        costs->vals[(mask - 1) * costs->m + v] = distances->vals[v * distances->m + u];
 
     }
 
@@ -37,52 +39,34 @@ __global__ void dw_fill_base_cases(cudatable::table_t* costs, cudagraph::graph_t
 
 __global__ void dw_fill_kth_combination(cudatable::table_t* costs, cudagraph::graph_t* g, cudatable::table_t* distances, cudaquery::query_t* terminals, int32_t k) {
 
-    int32_t v = blockDim.x * blockIdx.x + threadIdx.x;
-    int32_t w = blockDim.y * blockIdx.y + threadIdx.y;
+    // Assign the position in the table that the block will update.
 
-    if(v < g->vrt && w < g->vrt) {
-        
-        uint64_t mask = 0;
+    int32_t v = blockIdx.x;
+    int32_t i = blockIdx.y;
 
-        while( next_combination(terminals->size, k, &mask) ) {
+    // Declare and initialize minimum shared memory array.
 
-            int32_t exists = 0;
-            int32_t position = 0;
+    __shared__ float minimums[BLOCK_SIZE];
+    
+    minimums[threadIdx.x] = FLT_MAX;
 
-            for(int32_t i=0; i<terminals->size; i++) {
+    // Compute the mask of the current block
 
-                if(w == terminals->vals[i] && ((mask >> (terminals->size - i - 1)) & 1) == 1 ) {
+    uint64_t mask = ith_combination(terminals->size, k, i);
 
-                    exists = 1;
-                    position = i;
+    // Compute the minimum steiner tree
 
-                }
+    for(int32_t w = threadIdx.x; w<gridDim.x; w+=blockDim.x) {
 
-            }
+        int32_t exists = 0;
+        int32_t position = 0;
 
-            __syncthreads();
+        for(int32_t i=0; i<terminals->size; i++) {
 
+            if(w == terminals->vals[i] && ((mask >> (terminals->size - i - 1)) & 1) == 1 ) {
 
-            if(exists) {
-
-                uint64_t submask = 1ll << (terminals->size - position - 1);
-
-                float cost = distances->vals[v * distances->m + w] 
-                           + costs->vals[w * costs->m + ((mask & ~submask) - 1)];
-
-                atomicMin(&costs->vals[v * costs->m + mask - 1], cost);
-
-            } else if(g->deg[w] >= 3) {
-
-                for(uint64_t submask = (mask - 1) & mask; submask != 0; submask = (submask - 1) & mask) { // iterate over submasks of the mask O(2^T)
-
-                    float cost = distances->vals[v * distances->m + w]
-                               + costs->vals[w * costs->m + submask - 1]
-                               + costs->vals[w * costs->m + (mask & ~submask) - 1];
-
-                    atomicMin(&costs->vals[v * costs->m + mask - 1], cost);
-
-                }
+                exists = 1;
+                position = i;
 
             }
 
@@ -90,7 +74,52 @@ __global__ void dw_fill_kth_combination(cudatable::table_t* costs, cudagraph::gr
 
         }
 
-    } 
+        if(exists) {
+
+            uint64_t submask = 1ll << (terminals->size - position - 1);
+
+            float cost = distances->vals[v * distances->m + w] 
+                           + costs->vals[((mask & ~submask) - 1) * costs->m + w];
+
+            minimums[threadIdx.x] = (cost < minimums[threadIdx.x]) ? cost : minimums[threadIdx.x];
+
+        } else if(g->deg[w] >= 3) {
+
+            for(uint64_t submask = (mask - 1) & mask; submask != 0; submask = (submask - 1) & mask) {
+
+                float cost = distances->vals[v * distances->m + w]
+                               + costs->vals[(submask - 1) * costs->m + w]
+                               + costs->vals[((mask & ~submask) - 1) * costs->m + w];
+
+                minimums[threadIdx.x] = (cost < minimums[threadIdx.x]) ? cost : minimums[threadIdx.x]; //TODO: Do I need to syncthreads here?
+
+            }
+
+        }
+
+        __syncthreads();
+
+    }
+
+    // Parallel minimum reduction
+
+    for(int i=2; i<BLOCK_SIZE; i*=2) {
+
+        if(threadIdx.x % i == 0) {
+
+            minimums[threadIdx.x] = (minimums[threadIdx.x] < minimums[threadIdx.x + i / 2]) ? minimums[threadIdx.x] : minimums[threadIdx.x + i / 2];
+
+        }
+
+    }
+
+    // Assign the minimum to the corresponding slot in the table.
+
+    if(threadIdx.x == 0) {
+    
+        costs->vals[(mask - 1) * costs->m + v] = minimums[0];
+    
+    }
 
 }
 
@@ -117,21 +146,18 @@ void base_case(cudatable::table_t* table, cudagraph::graph_t* g, int32_t g_size,
 }
 
 
-/**
- * Works only for values of V < 2^21
- */
 void fill_kth_combination(cudatable::table_t* table, cudagraph::graph_t* g, int32_t g_size, cudaquery::query_t* t, int32_t t_size, cudatable::table_t* distances, int32_t k) {
 
-    int32_t num_thread_x = g_size;
-    int32_t num_thread_y = g_size;
+    initialize_factorial_table();
 
-    int32_t num_blocks_x = (num_thread_x + BLOCK_2D_SIZE - 1) / BLOCK_2D_SIZE;
-    int32_t num_blocks_y = (num_thread_y + BLOCK_2D_SIZE - 1) / BLOCK_2D_SIZE;
+    dim3 num_threads_per_block(BLOCK_SIZE);
 
-    dim3 num_thread_per_block(BLOCK_2D_SIZE, BLOCK_2D_SIZE);
+    int64_t num_blocks_x = g_size;
+    int64_t num_blocks_y = nCr(t_size, k);
+
     dim3 num_blocks(num_blocks_x, num_blocks_y);
 
-    dw_fill_kth_combination<<<num_blocks, num_thread_per_block>>>(table, g, distances, t, k);
+    dw_fill_kth_combination<<<num_blocks, num_threads_per_block>>>(table, g, distances, t, k);
 
     cudaError_t err = cudaDeviceSynchronize();
 
@@ -140,11 +166,12 @@ void fill_kth_combination(cudatable::table_t* table, cudagraph::graph_t* g, int3
         exit(err);
     }
 
-
 }
 
 
 void fill_steiner_tree_cuda_table(cudatable::table_t* table, cudagraph::graph_t* g, int32_t g_size, cudaquery::query_t* t, int32_t t_size, cudatable::table_t* distances) {
+
+    // Calculate base case
 
     base_case(table, g, g_size, t, t_size, distances);
 
@@ -168,11 +195,11 @@ void steiner_tree_gpu(cudagraph::graph_t* graph, int32_t nvrt, cudaquery::query_
 
     // Construct the costs table.
 
-    cudatable::make(&costs_d, nvrt, (int32_t) pow(2, nterm) - 1);
+    cudatable::make(&costs_d, (int32_t) pow(2, nterm) - 1, nvrt);
 
     // Fill the costs table.
 
-    fill_steiner_tree_cuda_table(costs_d, graph, nvrt, terminals, nterm, distances);
+    TIME(fill_steiner_tree_cuda_table(costs_d, graph, nvrt, terminals, nterm, distances), "\tDW GPU:");
 
     // Get the filled table from the GPU.
 
@@ -186,10 +213,10 @@ void steiner_tree_gpu(cudagraph::graph_t* graph, int32_t nvrt, cudaquery::query_
 
     (*result)->cost = FLT_MAX;
     
-    for(int32_t i=costs->m - 1; i < costs->m * costs->n; i = i + costs->m) {
-       
-        if(costs->vals[i] < (*result)->cost) {
-            (*result)->cost = costs->vals[i];
+    for(int32_t i=0; i < costs->m; i++) {
+
+        if(costs->vals[costs->m *(costs->n - 1) + i] < (*result)->cost) {
+            (*result)->cost = costs->vals[costs->m *(costs->n - 1) + i];
         }
 
     }
