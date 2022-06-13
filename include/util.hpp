58d33e29b83a8e0dfde523e59383559ff1ee91e6
atabou <andretabourian@hotmail.com>
2022-06-13 19:20:25 +0300

    Removed unused function.

--- include/util.hpp
+++ include/util.hpp
@@ -13,14 +13,5 @@
 
     #define TIME(EXPR, STR) CLOCKMACRO = clock(); EXPR; printf(STR); printf(" %fs\n", (double) (clock() - CLOCKMACRO) / CLOCKS_PER_SEC)
 
-    /**
-     * @brief Compares the value in the target pointer with the provided value, and saves the minimum between the two atomically.
-     * 
-     * @param target A pointer to a floating point value.
-     * @param val The value containing a potential new minimum.
-     * @return The old value of the pointer.
-     */
-    __device__ float atomicMin(float* target, float val);
-
 #endif
 /**@}*/
--- src/util.cu
+++ src/util.cu
@@ -1,19 +1,3 @@
 #include "util.hpp"
 
-__device__ float atomicMin(float* target, float val) {
 
-    int32_t ret = __float_as_int(*target);
-
-    while(val < __int_as_float(ret)) {
-
-        int32_t old = ret;
-        ret = atomicCAS((int32_t*) target, old, __float_as_int(val));
-        if(ret == old){
-            break;
-        }
-
-    }
-
-    return __int_as_float(ret);
-
-}
