742558c7bf45ebeefb10036a914013aad937a93d
atabou <andretabourian@hotmail.com>
2022-06-15 00:09:25 +0300

    Deleted folder

--- script/steiner.py
+++
@@ -1,9 +0,0 @@
-
-import ctypes
-
-if __name__ == "__main__":
-    libname = "./lib/libsteiner.so"
-    print(libname)
-    steiner = ctypes.CDLL(libname)
-
-    ctypes
--- script/tasks.py
+++
@@ -1,7 +0,0 @@
-import cffi
-
-print_banner("Building CFFI Module")
-ffi = cffi.FFI()
-
-include = "../include"
-headers = ["graph.hpp", "query.hpp", "table.hpp", ]
